b06845987e7cbc0f85ac8e240dd40cbbdf1b0a62
Kuaruou <linfanhsin@gmail.com>
2019-12-13 12:57:59 +0800

    fix card img sizing

--- js/all.js
+++ js/all.js
@@ -47,8 +47,8 @@ function loadAll() {
             '<div class="col-sm-6 col-md-4 col-lg-3 mb-3">' +
             '<div class="card-zoom h-100">'+ 
             '<div class="card bg-light h-100">' +
-            '<div class="card-body d-flex flex-column position-relative p-0">' +
-            '<img class="card-img-top modal-img-sizing";" src="' + data[i].Picture + '" alt="">' +
+            '<div class="card-body d-flex flex-column position-relative p-3">' +
+            '<img class="card-img-top";" src="' + data[i].Picture + '" alt="">' +
             '<span class="card-title"><b>' + data[i].Name + '</b></span>' +
             '<span class="card-span"><i class="fas fa-mortar-pestle"></i><b>類型:</b>' + data[i].Type + '</span>' +
             '<span class="card-span"><i class="fas fa-map-marker-alt" style="width: 18px"></i><b>地址:</b>' + data[i].Address + '</span>' +
@@ -74,8 +74,8 @@ function loadType(type) {
                 '<div class="col-sm-6 col-md-4 col-lg-3 mb-3">' +
                 '<div class="card-zoom h-100">'+
                 '<div class="card bg-light h-100">' +
-                '<div class="card-body d-flex flex-column">' +
-                '<img class="card-img-top modal-img-height" src="' + item.Picture + '" alt="">' +
+                '<div class="card-body d-flex flex-column position-relative p-3">' +
+                '<img class="card-img-top" src="' + item.Picture + '" alt="">' +
                 '<span class="card-title"><b>' + item.Name + '</b></span>' +
                 '<span class="card-span"><i class="fas fa-mortar-pestle"></i><b>類型:</b>' + item.Type + '</span>' +
                 '<span class="card-span"><i class="fas fa-map-marker-alt" style="width: 18px"></i><b>地址:</b>' + item.Address + '</span>' +
